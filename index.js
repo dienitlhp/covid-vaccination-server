36462089e6cf3bcb4f34596a17d410479bc064e2
Djinn <dienitlhp@gmail.com>
2021-08-06 01:26:14 +0700

    First commnit server

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = 8000;
+const data = require('./data.json')
+
+const fs = require('fs')
+
+app.use(cors());
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({limit: '50mb'}));
+
+app.get('/', (req, res) => {
+  res.send(data)
+});
+
+app.post('/', (req, res) => {
+  const jsonString = JSON.stringify(req.body.data);
+  fs.writeFileSync('./data.json', jsonString)
+
+  res.send('OK');
+})
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
